7ab50b809ab8fdfd398d999b5ff69ecb4b65b6cf
yangkeloff <yangkeloff@outlook.com>
2019-05-22 12:14:54 +0800

    gitalk-server

--- client/src/components/Comment_box.vue
+++ client/src/components/Comment_box.vue
@@ -17,26 +17,28 @@ export default {
     direct = this.direct
   },
   mounted() {
-    let gitalk = new Gitalk({
-      clientID: '565103516f4c64fdb1ca',
-      clientSecret: '1ed11dc9ff2228e9d09804e890bd94d60ffd87c3',
-      repo: 'my-blog',
-      owner: 'yangkeloff',
-      admin: ['yangkeloff'],
-      id: md5(window.location.href),
-      distractionFreeMode: false,
-      body: '文章地址：' + window.location.href,
-      title: this.title + " | yang's blog"
-    })
-    gitalk.render('gitalk_comment')
+    if(direct == false){
+      let gitalk = new Gitalk({
+        clientID: 'b7bcb4d28ab13ff8d743',
+        clientSecret: '238a6d133df2a9c6dfddaf7b222400fee58ce88b',
+        repo: 'my-blog',
+        owner: 'yangkeloff',
+        admin: ['yangkeloff'],
+        id: md5(window.location.href),
+        distractionFreeMode: false,
+        body: '文章地址：' + window.location.href,
+        title: this.title + " | yang's blog"
+      })
+      gitalk.render('gitalk_comment')
+    }
   },
   directives:{
     initComment: {
       inserted(el){
         if(direct == true){
           let gitalk = new Gitalk({
-            clientID: '565103516f4c64fdb1ca',
-            clientSecret: '1ed11dc9ff2228e9d09804e890bd94d60ffd87c3',
+            clientID: 'b7bcb4d28ab13ff8d743',
+            clientSecret: '238a6d133df2a9c6dfddaf7b222400fee58ce88b',
             repo: 'my-blog',
             owner: 'yangkeloff',
             admin: ['yangkeloff'],
